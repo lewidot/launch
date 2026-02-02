062433d88be5083c99f862fd863f9dfe2b178587
Lewis Evans <lewisevansuk@gmail.com>
2026-02-02 12:07:25 +0000

    proxy api requests to server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+	plugins: [svelte()],
+	build: {
+		outDir: '../server/public',
+		emptyOutDir: true
+	},
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+				changeOrigin: true
+			}
+		}
+	}
+});
