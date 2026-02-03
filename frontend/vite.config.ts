844f4ddd63ce81bd1412520306ea0b2338b3fa96
Lewis Evans <lewisevansuk@gmail.com>
2026-02-03 10:09:51 +0000

    add tailwindcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-	plugins: [svelte()],
-	build: {
-		outDir: '../server/public',
-		emptyOutDir: true
-	},
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:3000',
-				changeOrigin: true
-			}
-		}
-	}
+  plugins: [tailwindcss(), svelte()],
+  build: { outDir: "../server/public", emptyOutDir: true },
+  server: {
+    proxy: {
+      "/api": { target: "http://localhost:3000", changeOrigin: true },
+    },
+  },
 });
