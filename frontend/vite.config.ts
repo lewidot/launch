82d57f1ab89ae28770f733c1be13e6b45ff1962d
Lewis Evans <lewisevansuk@gmail.com>
2026-02-03 10:22:47 +0000

    add shadcn-svelte

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,19 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
-  build: { outDir: "../server/public", emptyOutDir: true },
-  server: {
-    proxy: {
-      "/api": { target: "http://localhost:3000", changeOrigin: true },
-    },
-  },
+	plugins: [tailwindcss(), svelte()],
+	build: { outDir: '../server/public', emptyOutDir: true },
+	server: {
+		proxy: {
+			'/api': { target: 'http://localhost:3000', changeOrigin: true }
+		}
+	},
+	resolve: {
+		alias: {
+			$lib: path.resolve('./src/lib')
+		}
+	}
 });
